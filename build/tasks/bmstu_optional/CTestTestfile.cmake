c82d2d484fe63005743a67c6e792947dd7e55e00
boynxtd00r <lukoshka08@gmail.com>
2025-12-13 18:33:36 +0300

    Коммит 1

---
+++ build/tasks/bmstu_optional/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/boynxtd00r/projects/bmstu_cpp_course/tasks/bmstu_optional
+# Build directory: /home/boynxtd00r/projects/bmstu_cpp_course/build/tasks/bmstu_optional
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
